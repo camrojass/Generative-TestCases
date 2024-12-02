e549d4332a0032a86f394796bdc5e6096491ce48
Camilo Rojas <100396227+camrojass@users.noreply.github.com>
2024-12-01 23:23:35 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,23 @@ os.environ["OPENAI_API_KEY"] = "key_openIA"
 PINECONE_API_KEY = "key_pinecone"
 PINECONE_ENV = "gcp-starter"
 
-
 # Variables for VectorDataUse
-chunk_size=1000
-chunk_overlap=200
+chunk_size = 1000
+chunk_overlap = 200
 
+#OpenAI
+model = "gpt-3.5-turbo"
+temperature = 0.2
+token = 150
 
 # Variables for PineconeUse
-metric="cosine"
-dimension=1536
-index_name = "chatgptdbvector"
+metric = "cosine"
+dimension = 20000
+index_name = "dbindex"
+cloud = "aws"
+region = "us-east-1"
+
+# Variables archivos locales
+input_file_path = "C:\\Repos\\Proyect\\input\\OpenAPI.pdf"
+output_file_path = "C:\\Repos\\Proyect\\output\\postmanCollection.json"
+output_excel_file_path = "C:\\Repos\\Proyect\\output\\testCases.xlsx"
